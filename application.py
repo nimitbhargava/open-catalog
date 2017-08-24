199fc6218a720480c2d732860240bc3d7221d2e8
Nimit Bhargava <nimitbhargava@gmail.com>
2017-08-25 01:31:22 +0530

    Logging out from Google implementation

--- application.py
+++ application.py
@@ -146,9 +146,17 @@ def login():
 
 
 # Logout
-@app.route('/login')
+@app.route('/logout')
 def logout():
-    return "Logout Page"
+    if login_session['provider'] == 'google':
+        gdisconnect()
+        del login_session['gplus_id']
+        del login_session['access_token']
+    del login_session['username']
+    del login_session['email']
+    del login_session['user_id']
+    del login_session['provider']
+    return redirect(url_for('show_categories'))
 
 
 # Google Login
@@ -235,7 +243,23 @@ def gconnect():
 # Google Logout
 @app.route('/gdisconnect')
 def gdisconnect():
-    return "g disconnect Page"
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] != '200':
+        # For whatever reason, the given token was invalid.
+        response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 # APIs
